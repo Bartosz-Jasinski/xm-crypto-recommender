a3778864a43eb83e0b19b984e1631cf2bcb58593
Bartosz-Jasinski <Bartosz_Jasinski@epam.com>
2023-04-27 13:06:48 +0200

    Init CryptoRecommender project

---
+++ src/main/java/com/epam/recommendationserice/CryptoRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.epam.recommendationserice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/recommendationserice/CryptoRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.recommendationserice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
