601842fbd8f46c24bca130664c73fe25b49c2d24
Bartosz-Jasinski <Bartosz_Jasinski@epam.com>
2023-04-27 13:08:46 +0200

    Fix package name typo

--- src/main/java/com/epam/recommendationservice/CryptoRecommenderApplication.java
+++ src/main/java/com/epam/recommendationservice/CryptoRecommenderApplication.java
@@ -1,4 +1,4 @@
-package com.epam.recommendationserice;
+package com.epam.recommendationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
